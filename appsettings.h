dd72144f2caa7b7e245b7157fbf3ae4067b24c02
NRG-Drink <beatdz93@hotmail.com>
2024-01-15 16:18:52 +0100

    init: basic files

---
+++ appsettings.h
@@ -0,0 +1,7 @@
+
+const int PRESET_1 = 47;
+const int PRESET_2 = 45;
+const int USER = 49;
+
+const int LOOP_DURATION_MS = 25;
+const int SWITCH_LONG_PUSH_MS = 500;
---
+++ table-adjuster.ino
@@ -0,0 +1,10 @@
+#include "appsettings.h"
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  delay(LOOP_DURATION_MS);
+}
